588036294856e927aa5be4496d80bc681a65577c
Xelyz <wilson.wu.you@icloud.com>
2025-07-18 16:09:23 +0800

    cursor idk what you have done

--- Assets/Script/Game/NoteScroll.cs
+++ Assets/Script/Game/NoteScroll.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 public class NoteScroll : MonoBehaviour
 {
     public bool isActive = true;
-    private float timeOffset;
-    private float initialZ;
+    // private float timeOffset;
+    // private float initialZ;
 
     void OnEnable()
     {
-        initialZ = transform.position.z;
+        // initialZ = transform.position.z;
         // 计算从当前位置到判定线所需的时间
-        timeOffset = (initialZ - Values.planeDistance) / Values.Preference.NoteSpeed;
+        // why did this exist? cursor?!
+        // timeOffset = (initialZ - Values.planeDistance) / Values.Preference.NoteSpeed;
     }
 
     void Update()
@@ -18,8 +19,8 @@ public class NoteScroll : MonoBehaviour
         if (GameManager.Instance.gameState.IsPlaying && isActive)
         {
             // 使用补偿后的时间来计算位置
-            float compensatedSpeed = Values.Preference.NoteSpeed * (1 + timeOffset * 0.01f);
-            transform.position -= new Vector3(0, 0, compensatedSpeed * Time.deltaTime);
+            // float compensatedSpeed = Values.Preference.NoteSpeed * (1 + timeOffset * 0.01f);
+            transform.position -= new Vector3(0, 0, Values.Preference.NoteSpeed * Time.deltaTime);
         }
     }
 }
